ec85aedbcc3df3c78fc2fb2e40fbd6daa24a282a
Your Name <you@example.com>
2022-09-05 11:08:10 -0500

    commenting added to index.js and formating

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 inquirer
+  //collecting data from user
   .prompt([
     {
       type: "input",
@@ -11,8 +12,7 @@ inquirer
     },
     {
       type: "input",
-      message:
-        "Now what is the exact title of the REPO for your project?",
+      message: "Now what is the exact title of the REPO for your project?",
       name: "projectLink",
     },
     {
@@ -34,8 +34,7 @@ inquirer
     },
     {
       type: "input",
-      message:
-        "I see. Is there any usage information we should have?",
+      message: "I see. Is there any usage information we should have?",
       name: "usageInfo",
     },
     {
@@ -49,23 +48,38 @@ inquirer
       name: "testInstruct",
     },
     {
-        //need to figure out how to do this options thing
+      //creating a list of options to select the license
       type: "list",
       name: "license",
-      message: "Almost finished! What license is your project covered under? Select an option below.",
-      choices: ["GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+      message:
+        "Almost finished! What license is your project covered under? Select an option below.",
+      choices: [
+        "GNU General Public License v3.0",
+        "MIT License",
+        "BSD 2-Clause 'Simplified' License",
+        "BSD 3-Clause 'New' or 'Revised' License",
+        "Boost Software License 1.0",
+        "Creative Commons Zero v1.0 Universal",
+        "Eclipse Public License 2.0",
+        "GNU Affero General Public License v3.0",
+        "GNU General Public License v2.0",
+        "GNU Lesser General Public License v2.1",
+        "Mozilla Public License 2.0",
+        "The Unlicense",
+      ],
     },
     {
-        type: "input",
-        message: "Cool. Now, what is your email address?",
-        name: "email",
-      },
+      type: "input",
+      message: "Cool. Now, what is your email address?",
+      name: "email",
+    },
     {
-        type: "input",
-        message: "Lastly, what is your GitHub user name?",
-        name: "gitHubName",
-      },
+      type: "input",
+      message: "Lastly, what is your GitHub user name?",
+      name: "gitHubName",
+    },
   ])
+  //checking answers and passing the user's answers into a const
   .then((answers) => {
     console.log(answers);
     const {
@@ -79,47 +93,50 @@ inquirer
       testInstruct,
       license,
       email,
-      gitHubName
+      gitHubName,
     } = answers;
+    //clear out the licenseLink each time and have a var for the link to go once selected
     var licenseLink = "";
-
-if (answers.license == "GNU General Public License v3.0"){
-    licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
-}
-if (answers.license == "MIT License"){
-   licenseLink = "https://opensource.org/licenses/MIT"
-}
-if (answers.license == "BSD 2-Clause 'Simplified' License"){
-    licenseLink = "https://opensource.org/licenses/BSD-2-Clause"
- }
- if (answers.license == "BSD 3-Clause 'New' or 'Revised' License"){
-    licenseLink = "https://en.wikipedia.org/wiki/BSD_licenses#3-clause_license_(%22BSD_License_2.0%22,_%22Revised_BSD_License%22,_%22New_BSD_License%22,_or_%22Modified_BSD_License%22)"
- }
- if (answers.license == "Boost Software License 1.0"){
-    licenseLink = "https://www.boost.org/LICENSE_1_0.txt"
- }
- if (answers.license == "Creative Commons Zero v1.0 Universal"){
-    licenseLink = "https://creativecommons.org/publicdomain/zero/1.0/"
- }
- if (answers.license == "Eclipse Public License 2.0"){
-    licenseLink = "https://www.eclipse.org/legal/epl-2.0/"
- }
- if (answers.license == "GNU Affero General Public License v3.0"){
-    licenseLink = "https://www.gnu.org/licenses/agpl-3.0.en.html"
- }
- if (answers.license == "GNU General Public License v2.0"){
-    licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html"
- }
- if (answers.license == "GNU Lesser General Public License v2.1"){
-    licenseLink = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html"
- }
- if (answers.license == "Mozilla Public License 2.0"){
-    licenseLink = "https://www.mozilla.org/en-US/MPL/2.0/"
- }
- if (answers.license == "The Unlicense"){
-    licenseLink = "https://unlicense.org/"
- }
-
+    //if statements that associate the licence choice with it's matching html page
+    if (answers.license == "GNU General Public License v3.0") {
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+    }
+    if (answers.license == "MIT License") {
+      licenseLink = "https://opensource.org/licenses/MIT";
+    }
+    if (answers.license == "BSD 2-Clause 'Simplified' License") {
+      licenseLink = "https://opensource.org/licenses/BSD-2-Clause";
+    }
+    if (answers.license == "BSD 3-Clause 'New' or 'Revised' License") {
+      licenseLink =
+        "https://en.wikipedia.org/wiki/BSD_licenses#3-clause_license_(%22BSD_License_2.0%22,_%22Revised_BSD_License%22,_%22New_BSD_License%22,_or_%22Modified_BSD_License%22)";
+    }
+    if (answers.license == "Boost Software License 1.0") {
+      licenseLink = "https://www.boost.org/LICENSE_1_0.txt";
+    }
+    if (answers.license == "Creative Commons Zero v1.0 Universal") {
+      licenseLink = "https://creativecommons.org/publicdomain/zero/1.0/";
+    }
+    if (answers.license == "Eclipse Public License 2.0") {
+      licenseLink = "https://www.eclipse.org/legal/epl-2.0/";
+    }
+    if (answers.license == "GNU Affero General Public License v3.0") {
+      licenseLink = "https://www.gnu.org/licenses/agpl-3.0.en.html";
+    }
+    if (answers.license == "GNU General Public License v2.0") {
+      licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html";
+    }
+    if (answers.license == "GNU Lesser General Public License v2.1") {
+      licenseLink =
+        "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html";
+    }
+    if (answers.license == "Mozilla Public License 2.0") {
+      licenseLink = "https://www.mozilla.org/en-US/MPL/2.0/";
+    }
+    if (answers.license == "The Unlicense") {
+      licenseLink = "https://unlicense.org/";
+    }
+    //Template Literal that get's printed with user's answers
     const readMe = `# ${title}
     ${license}
 ## Links
@@ -154,13 +171,15 @@ If you have questions about this project or any of my other work, please contact
     
 ## License Info
 This project is covered under **${license}**. Follow the link to learn more about <a href="${licenseLink}" target="_blank">*${license}*</a>.`;
-
+    //writes the readme file and notifies user whether it was successful or not
     fs.writeFile("README.md", readMe, (err) =>
-      err ? console.log(err) : console.log("Success! Your README has been generated. Have a great day!")
+      err
+        ? console.log(err)
+        : console.log(
+            "Success! Your README has been generated. Have a great day!"
+          )
     );
   });
 
-//readme formatting docs from github:
-  //https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
-
-  //https://youtu.be/qcAhijXro-Q
+//link to my video tutorial
+//https://youtu.be/qcAhijXro-Q
